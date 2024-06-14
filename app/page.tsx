3ebb7a0b847fbdde707f57710cafa857733f4ffc
Juan Morais <timrodz@icloud.com>
2024-06-14 23:41:22 +1200

    hero + animations + layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,58 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
 import { projects, about, socials } from "./data";
+import { motion, useMotionValue, useTransform } from "framer-motion";
+
+export const MotionDiv = motion.div;
 
 export default function Home() {
+  const heroDrag = useMotionValue(0);
+  const backgroundColor = useTransform(
+    heroDrag,
+    [-100, 0, 100],
+    ["#85eed9", "#85eee2", "#85eaee"]
+  );
+  const heroTextColor = useTransform(
+    heroDrag,
+    [-200, 0, 200],
+    ["#19805c", "#0f766e", "#0f6d76"]
+  );
+  const heroTextColorLight = useTransform(
+    heroDrag,
+    [-200, 0, 200],
+    ["#0d9460", "#0d9488", "#0d8294"]
+  );
+
   return (
     <main>
-      <section id="hero" className="hero-wrapper">
+      <MotionDiv id="hero" className="hero-wrapper" style={{ backgroundColor }}>
         <div
           id="hero-container"
-          className="max-w-[100%] md:max-w-screen-xl mx-auto px-4 md:px-0"
+          className="max-w-screen-xl mx-auto px-4 md:px-6 lg:px-0"
         >
-          <div
+          <MotionDiv
             id="hero-contents"
-            className="flex flex-col gap-6 md:gap-8 text-teal-800"
+            className="flex flex-col gap-6 md:gap-8"
+            style={{
+              color: heroTextColor,
+            }}
           >
-            <h1 className="text-3xl md:text-8xl font-semibold">{about.name}</h1>
-            <h2 className="text-2xl md:text-7xl text-teal-700">
-              {about.title}
-            </h2>
+            <MotionDiv
+              id="hero-name"
+              drag="x"
+              dragConstraints={{ left: 0, right: 0 }}
+              whileTap={{ scale: 1.05 }}
+              style={{ x: heroDrag }}
+            >
+              <h1 className="hero-name text-3xl md:text-8xl font-semibold hover:underline hover:cursor-pointer">
+                {about.name}
+              </h1>
+            </MotionDiv>
+            <MotionDiv style={{ color: heroTextColorLight }}>
+              <h2 className="text-2xl md:text-7xl">{about.title}</h2>
+            </MotionDiv>
             <div id="hero-socials" className="flex gap-4">
               {socials.map(({ name, url, icon }) => (
                 <Link key={`socials-${name}`} href={url} target="_blank">
@@ -28,13 +63,18 @@ export default function Home() {
             <Link href="#content">
               <button className="hero-button">See my work</button>
             </Link>
-          </div>
+          </MotionDiv>
         </div>
-      </section>
-      <div id="content" className="max-w-screen-xl mx-auto p-6 md:p-0">
-        <section id="projects" className="pt-10">
-          <h2 className="text-2xl font-semibold mb-3">Portfolio</h2>
-          <p className="subtext">{about.portfolio}</p>
+      </MotionDiv>
+      <MotionDiv
+        id="content"
+        className="max-w-screen-xl mx-auto px-6 py-24 md:px-0 md:py-32"
+      >
+        <div id="projects" className="mt-12 flex flex-col gap-10">
+          <div id="projects-title">
+            <h2 className="text-5xl font-semibold mb-6">Portfolio</h2>
+            <p className="subtext">{about.portfolio}</p>
+          </div>
           <div
             id="project-grid"
             className="mt-6 grid grid-cols-1 md:grid-cols-3 gap-6"
@@ -62,8 +102,8 @@ export default function Home() {
               );
             })}
           </div>
-        </section>
-      </div>
+        </div>
+      </MotionDiv>
     </main>
   );
 }
