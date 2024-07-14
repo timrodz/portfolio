8e66345333135b1e677a7f0af6063c34e0de34d4
Juan Morais <timrodz@icloud.com>
2024-07-14 23:08:11 +1200

    feat: Testimonials

---
+++ app/components/Testimonials.tsx
@@ -0,0 +1,37 @@
+import { testimonials } from "@data";
+import Image from "next/image";
+import { Fragment } from "react";
+
+export const Testimonials = () => {
+  return (
+    <section id="testimonials">
+      <h2>Testimonials</h2>
+      <hr />
+      <p>
+        Throughout my career, I’ve had the privilege of working with many
+        talented people. Working with them has yielded wonderful experiences and
+        partnerships.
+      </p>
+      <div className="testimonial-container mt-10">
+        {testimonials.map((t) => (
+          <div key={t.name}>
+            <div className="testimonial-person">
+              <Image
+                src={t.imageUrl}
+                alt={t.name}
+                width={100}
+                height={100}
+                className="rounded-full aspect-square object-cover"
+              />
+              <div>
+                <h3>{t.name}</h3>
+                <h4>{t.title}</h4>
+              </div>
+            </div>
+            <blockquote className="testimonial">{t.summary}</blockquote>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
